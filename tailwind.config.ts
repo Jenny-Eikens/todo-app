4f2e1d0caebadac4007cc1ffb633962e39e337c9
Jenny-Eikens <jennifereikens@web.de>
2025-01-04 18:34:20 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -8,11 +9,38 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        lighttheme: {
+          primary: "#fff",
+        },
+        darktheme: {
+          primary: "#aaa",
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
 } satisfies Config;
+
+/*
+light theme:
+- Very Light Gray: hsl(0, 0%, 98%)
+- Very Light Grayish Blue: hsl(236, 33%, 92%)
+- Light Grayish Blue: hsl(233, 11%, 84%)
+- Dark Grayish Blue: hsl(236, 9%, 61%)
+- Very Dark Grayish Blue: hsl(235, 19%, 35%)
+
+dark theme:
+- Very Dark Blue: hsl(235, 21%, 11%)
+- Very Dark Desaturated Blue: hsl(235, 24%, 19%)
+- Light Grayish Blue: hsl(234, 39%, 85%)
+- Light Grayish Blue (hover): hsl(236, 33%, 92%)
+- Dark Grayish Blue: hsl(234, 11%, 52%)
+- Very Dark Grayish Blue: hsl(233, 14%, 35%)
+- Very Dark Grayish Blue: hsl(237, 14%, 26%)
+*/
