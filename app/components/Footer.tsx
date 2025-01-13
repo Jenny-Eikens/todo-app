2d1074a0dfeee9fbca11d36bf2e489978a3921fa
Jenny-Eikens <jennifereikens@web.de>
2025-01-13 11:36:54 +0100

    Make items singular if only one todo is open

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -18,7 +18,9 @@ const Footer = ({
 }: FooterProps) => {
   return (
     <div className="mx-3 flex justify-between p-2 text-sm font-[700] text-[hsl(236,9%,61%)] dark:text-[hsl(234,11%,52%)]">
-      <span>{count} items left</span>
+      <span>
+        {count} {count === 1 ? "item" : "items"} left
+      </span>
       <div className="hidden space-x-8 md:inline">
         <Filter
           onShowAll={onShowAll}
